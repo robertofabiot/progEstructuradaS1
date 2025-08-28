5a5b53ff4c1bce55aa1a4000328f702e8073d3a8
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 11:20:46 -0600

    Add ejercicio2

---
+++ sesion1/ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Desarrolle un programa que calcule el tiempo total en horas de un viaje en bus desde Managua a Granada, donde el usuario ingresa la velocidad promedio en kilómetros por hora como entrada, asumiendo una distancia fija de 40 kilómetros.
+
+Console.WriteLine("=====CALCULADORA DEL TIEMPO DE VIAJE EN BUS=====");
+int distancia = 40;
+
+// v = d / t  => t = d / v
+
+Console.Write("Ingrese la velocidad promedio del bus en km/h: ");
+double velocidad = Convert.ToDouble(Console.ReadLine());
+
+double tiempoHoras = distancia / velocidad;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"El tiempo total del viaje es: {tiempoHoras:F2} horas.");
+Console.ResetColor();
