a2d76869903f876d532acdb2d5f720a2f40cc412
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 23:43:15 -0600

    Add aprobo_reprobo switch

---
+++ ifswitch/aprobo_reprobo_switch/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Ingrese la calificación del estudiante: ");
+double calificacion = Convert.ToDouble(Console.ReadLine());
+
+switch (calificacion)
+{
+    case double n when (n >= 0 && n < 50):
+        Console.WriteLine("Reprobó");
+        break;
+
+    case double n when (n >= 50 && n <= 69):
+        Console.WriteLine("Recuperación");
+        break;
+
+    case double n when (n >= 70 && n <= 100):
+        Console.WriteLine("Aprobó");
+        break;
+
+    default:
+        Console.WriteLine("Valor inválido");
+        break;
+}
