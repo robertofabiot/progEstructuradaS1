df8b641dfeef76c1a3731f2aaa30aefbcd37a750
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 20:10:31 -0600

    Add ejercicio 4

---
+++ sesion1/ejercicio4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("¡Vamos a calcular el volumen del tanque de agua!");
+
+Console.Write("Ingrese la longitud del tanque: ");
+double longitud = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese el ancho del tanque: ");
+double ancho = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese la altura del tanque: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volumen = longitud * ancho * altura;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"El volumen del tanque es {volumen}");
+Console.ResetColor();
