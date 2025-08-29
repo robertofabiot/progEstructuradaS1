8e58168006825673d0cbb3b72b91ce9a7800c236
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-29 08:07:49 -0600

    Add ejercicio while

--- sesion3/carrera/Program.cs
+++ sesion3/carrera/Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.WriteLine("Ingrese un número según la carrera que estudia: ");
-int carrera = Convert.ToInt32(Console.ReadLine());
+string opcionCarrera = Console.ReadLine();
 
-switch (carrera)
+switch (opcionCarrera)
 {
-    case 1:
+    case "1":
         Console.WriteLine("El usuario estudia medicina.");
         break;
-    case 2:
+    case "2":
         Console.WriteLine("El usuario estudia sistemas.");
         break;
-    case 3:
+    case "3":
         Console.WriteLine("El usuario dejó de estudiar.");
         break;
     default:
---
+++ sesion3/while1/Program.cs
@@ -0,0 +1,14 @@
+﻿int num, suma = 0;
+Console.Write("Dime un #: ");
+string entrada = Console.ReadLine();
+num = Convert.ToInt32(entrada);
+
+while (num >= 0)
+{
+    suma += num;
+    Console.Write("Dime un #: ");
+    entrada = Console.ReadLine();
+    num = Convert.ToInt32(entrada);
+}
+
+Console.WriteLine("La suma es: " + suma);
