1fe0438eccceee4973302fbf948516cbb684d2e1
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 23:17:47 -0600

    Add positivo, negativo o cero

---
+++ ifswitch/if_positivo_negativo/Program.cs
@@ -0,0 +1,21 @@
+﻿//Determinar si un número ingresado es positivo, negativo o cero usando if.
+
+Console.Write("Ingrese un número: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine("El número es positivo");
+}
+else if (num < 0)
+{
+    Console.WriteLine("El número es negativo");
+}
+else if (num == 0)
+{
+    Console.WriteLine("El número es cero");
+}
+else
+{
+    Console.WriteLine("Valor inválido");
+}
