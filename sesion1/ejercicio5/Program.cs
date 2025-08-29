9e6c4a44e553b06815fd543496cb8b01cf05caf0
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 21:30:25 -0600

    Add ejercicio 5

---
+++ sesion1/ejercicio5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("===CALCULADORA DE LECHE====");
+Console.Write("Ingrese el volumen que produce una vaca en litros: ");
+double volumen = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese la cantidad de vacas ordeñadas: ");
+int vacas = Convert.ToInt32(Console.ReadLine());
+
+double lecheLitros = volumen * vacas;
+double lecheBaldes = lecheLitros / 20;
+
+Console.WriteLine($"La leche producida equivale a {lecheBaldes} baldes");
