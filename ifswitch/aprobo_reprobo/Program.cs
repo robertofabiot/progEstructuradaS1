73d05d2d08cd59c75389d10fe70288ccff66f65c
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 23:33:59 -0600

    Add aprobo reprobo if

---
+++ ifswitch/aprobo_reprobo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Evaluar la calificación de un estudiante y mostrar si aprobó (≥70), recuperación (50–69) o reprobó (<50) con if.
+
+Console.Write("Ingrese la calificación del estudiante: ");
+double calificacion = Convert.ToDouble(Console.ReadLine());
+
+if (calificacion >= 70 && calificacion <=100)
+{
+    Console.WriteLine("Aprobó");
+}
+else if (calificacion >= 50 && calificacion <= 69)
+{
+    Console.WriteLine("Recuperación");
+}
+else if (calificacion < 50 && calificacion >= 0)
+{
+    Console.WriteLine("Reprobó");
+}
+else
+{
+    Console.WriteLine("Valor inválido");
+}
