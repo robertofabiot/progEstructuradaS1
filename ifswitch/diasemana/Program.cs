a500982039d311bdce2a3018c56b475c02715d99
robertofabiot <rftercerom@uamv.edu.ni>
2025-08-28 23:20:54 -0600

    Add dia semana switch

---
+++ ifswitch/diasemana/Program.cs
@@ -0,0 +1,31 @@
+﻿int diaSemana = 0;
+Console.Write("Ingrese el número de día: ");
+diaSemana = int.Parse(Console.ReadLine());
+
+switch (diaSemana)
+{
+    case 1:
+        Console.Write("Lunes");
+        break;
+    case 2:
+        Console.Write("Martes");
+        break;
+    case 3:
+        Console.Write("Miércoles");
+        break;
+    case 4:
+        Console.Write("Jueves");
+        break;
+    case 5:
+        Console.Write("Viernes");
+        break;
+    case 6:
+        Console.Write("Sábado");
+        break;
+    case 7:
+        Console.Write("Domingo");
+        break;
+    default:
+        Console.Write("Ingrese un día válido.");
+        break;
+}
